c4a014d5e5bc3bf33a6fbe28470ebcecd5600a1a
Shusei-Dev <matissem33@gmail.com>
2022-05-31 09:55:05 +0200

    still working on the server part

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ import socketserver
 from socketserver import StreamRequestHandler, DatagramRequestHandler
 from socketserver import ThreadingTCPServer, TCPServer, ThreadingUDPServer, UDPServer
 from time import ctime
+import threading
 import socket
 
 
@@ -12,37 +13,69 @@ class TCPRequestHandler(StreamRequestHandler):
         return super().setup()
 
     def handle(self) -> None:
+        # Get the connection of a client
         client_ip = self.client_address[0]
         print("connected from " + client_ip)
 
+        # Get what send the client
         client_send_data_line = self.rfile.readline().strip()
-        client_send_data_line_str = client_send_data_line.decode('utf-8')
-        self.cmdPrompt.check_cmd(client_send_data_line_str, self)
+        client_data_str = client_send_data_line.decode('utf-8')
+
+        # Check what current login_state is the client
+        if "login_state" in getCDisVar(client_data_str) and getCDisVar(client_data_str)["login_state"] in ["1", "2"]:
+            self.client_login_state = getCDisVar(client_data_str)["login_state"]
+            self.wfile.write(b"1")
+
+        client_send_data_line = self.rfile.readline().strip()
+        client_data_str = client_send_data_line.decode('utf-8')
+
+        if self.client_login_state == "1" and "user_name" in getCDisVar(client_data_str):
+            print("AAAAAAA")
 
 
-        curr_time = ctime()
-        self.wfile.write((curr_time + ' - ' + client_send_data_line_str).encode('utf-8'))
+
+        #self.cmdPrompt.check_cmd(client_data_str, tcp_server)
+
+        # Send to the client data
+        #curr_time = ctime()
+        #self.wfile.write((curr_time + ' - ' + client_data_str).encode('utf-8'))
 
     def finish(self) -> None:
         return super().finish()
 
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+def getCDisVar(cd=str()):
+    if "=" in cd:
+        data_var_list = cd.split("=")
+
+    if len(data_var_list) == 2:
+        c = 0
+        for data in data_var_list:
+            data_var_list[c] = data.lstrip()
+            c += 1
+
+    return {data_var_list[0]: data_var_list[1]}
+
+
 class CommandPrompt():
 
     def __init__(self):
-        self.cmdDict = {"close": close_server}
+        self.cmdDict = {"close": self.close_server}
 
     def check_cmd(self, data, server):
         self.dataList = data.split()
         self.serverClass = server
 
-        if len(self.dataList) <= 1:
+        if len(self.dataList) <= 1 and self.dataList[0] in self.cmdDict.keys():
             self.cmdDict[self.dataList[0]](self.serverClass)
 
-class close_server:
-
-    def __init__(self, server):
+    def close_server(self, server):
         print("Closing the server...")
         server.shutdown()
+
+
 
 
 def create_tcp_server():
@@ -50,7 +83,9 @@ def create_tcp_server():
     server_port_number = 9999
 
     socketserver.TCPServer.allow_reuse_address = True
-    tcp_server = TCPServer((server_host, server_port_number), TCPRequestHandler)
+
+    global tcp_server
+    tcp_server = ThreadedTCPServer((server_host, server_port_number), TCPRequestHandler)
     tcp_running_message = 'TCP server is started on host \'' + server_host + ':' + str(server_port_number) + '\'\r\n'
     print(tcp_running_message)
     try:
